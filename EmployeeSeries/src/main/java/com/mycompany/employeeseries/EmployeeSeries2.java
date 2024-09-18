78496f1a394dd0ec16d1f19186976fb9f284a9ff
Jansen Choi Kai Xuan <140486215+choijans@users.noreply.github.com>
2024-09-18 17:42:37 +0800

    Add files via upload

---
+++ EmployeeSeries/src/main/java/com/mycompany/employeeseries/EmployeeSeries2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.employeeseries;
+import com.mycompany.employeeseries.version2.*;
+
+/**
+ *
+ * @author User
+ */
+public class EmployeeSeries2 {
+    public static void main(String[] args) {
+        Employee e1 = new Employee(10001, "Jans");
+        e1.displayEmployee();
+    }
+}
